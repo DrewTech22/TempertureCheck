20eaad874d08907b6c41eb355fb6d4e6ec08aa04
Andrew Chavis <95153684+DrewTech22@users.noreply.github.com>
2022-06-17 12:45:28 -0400

    Create temp.js

---
+++ temp.js
@@ -0,0 +1,7 @@
+const kelvin = 299; //Temperture in kelvin
+
+let celsius = kelvin - 273; // The equation for celsius
+
+var fahrenheit = celsius * (9/5) + 32; //equation is meant to work for fahrenheit
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
